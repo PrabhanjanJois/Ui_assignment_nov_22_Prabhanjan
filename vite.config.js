40d3c946e46270c872ae7af6c8e0a45648d57012
Prabhanjan-CC <prabhanjan.jois@cognitiveclouds.com>
2025-11-22 12:54:17 +0530

    config changes

--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          "vendor-react": ["react", "react-dom"],
-          "vendor-redux": ["react-redux", "@reduxjs/toolkit"],
-          "vendor-charts": ["recharts"],
-        },
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
       },
-    },
-    chunkSizeWarningLimit: 1000,
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-  },
+    }),
+  ],
 });
